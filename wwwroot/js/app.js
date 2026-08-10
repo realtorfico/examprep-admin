ae8c2dc24c30dcd7831fb62d3f5831a8f14e4aa3
Anand <avangari@hotmail.com>
2026-08-10 11:43:52 -0700

    Reorder DMV tracks to sit right after Notary in the tab order

--- wwwroot/js/app.js
+++ wwwroot/js/app.js
@@ -186,8 +186,9 @@ async function renderCodes() {
 
 // ---- Questions --------------------------------------------------------
 
-var EXAM_TYPES = [['notary', 'California Notary'], ['dre', 'California DRE'], ['mlo', 'National MLO'],
-  ['ca_driver', 'CA Driver (Class C)'], ['cdl', 'CA Commercial (CDL)'], ['motorcycle', 'CA Motorcycle (M1/M2)']];
+var EXAM_TYPES = [['notary', 'California Notary'],
+  ['ca_driver', 'CA Driver (Class C)'], ['cdl', 'CA Commercial (CDL)'], ['motorcycle', 'CA Motorcycle (M1/M2)'],
+  ['dre', 'California DRE'], ['mlo', 'National MLO']];
 var currentQuestionsExamType = 'notary';
 var currentQuestionsTopic = null; // null = "All"
 var questionsCache = []; // full (unfiltered-by-topic) list for the current exam type
