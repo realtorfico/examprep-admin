165ee75296bf05fba6bafcffec8da4e73477e9a2
Anand <avangari@hotmail.com>
2026-08-11 07:17:28 -0700

    Add fl_driver / fl_cdl to the Add Code exam-type dropdown

--- wwwroot/js/app.js
+++ wwwroot/js/app.js
@@ -189,6 +189,7 @@ async function renderCodes() {
 var EXAM_TYPES = [['ca_notary', 'California Notary'],
   ['ca_driver', 'CA Driver (Class C)'], ['ca_cdl', 'CA Commercial (CDL)'], ['ca_motorcycle', 'CA Motorcycle (M1/M2)'],
   ['tx_driver', 'TX Driver'], ['tx_cdl', 'TX Commercial (CDL)'],
+  ['fl_driver', 'FL Driver'], ['fl_cdl', 'FL Commercial (CDL)'],
   ['ca_dre', 'California DRE'], ['mlo', 'National MLO']];
 var currentQuestionsExamType = 'ca_notary';
 var currentQuestionsTopic = null; // null = "All"
